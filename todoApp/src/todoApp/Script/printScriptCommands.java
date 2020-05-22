43a939ae5b979627f82fb829ead8813d32e17768
gyim <gyim1345@gmail.com>
2020-05-22 10:12:39 +0900

    divide Script into 3 parts

--- todoApp/src/todoApp/Script/printScriptCommands.java
+++ todoApp/src/todoApp/Script/printScriptCommands.java
@@ -1,6 +1,6 @@
 package todoApp.Script;
 
-public class scriptCommands {
+public class printScriptCommands {
     private int depthOne;
     private int depthTwo;
 
@@ -37,5 +37,43 @@ public class scriptCommands {
         }
 
     }
+
+
+    public static void entryScript() {
+        System.out.println("\n----------------------");
+        System.out.println("1. todo 조작");
+        System.out.println("2. Completed todo 조작");
+        System.out.println("0. App 종지");
+        System.out.println("----------------------\n");
+        System.out.print("\n Input command number: ");
+    }
+
+    public static void todoScript() {
+        System.out.println("\n----------------------");
+        System.out.println("1. todo 등록");
+        System.out.println("2. todo 출력");
+        System.out.println("3. todo 완료");
+        System.out.println("4. todo 삭제");
+        System.out.println("5. todo 우선 순위 바꾸기");
+        System.out.println("6. todo 제목 바꾸기");
+        System.out.println("0. 되돌아가기");
+        System.out.println("----------------------\n");
+        System.out.print("\n Input command number: ");
+    }
+
+    public static void completedTodoScript() {
+        System.out.println("\n----------------------");
+        System.out.println("1. completedTodo 출력");
+        System.out.println("2. completedTodo 삭제");
+        System.out.println("0. 되돌아가기");
+        System.out.println("----------------------\n");
+        System.out.print("\n Input command number: ");
+    }
+
+
+
+
+
+
 
 }
